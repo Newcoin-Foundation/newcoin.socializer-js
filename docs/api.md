2c996bf2cd835e2983174d0c015ed7f4ff8cf419
NatPDeveloper <natanael.prudhomme@gmail.com>
2022-04-06 11:49:43 -0400

    add api docs, add balances tests

---
+++ docs/api.md
@@ -0,0 +1,68 @@
+***
+
+# getPayouts
+
+### Parameters:
+* @param owner - account to fetch balance for
+
+### Description:
+
+Return payouts object.
+
+### Cleos Example:
+
+```
+cleos -u https://nodeos.newcoin.org get table social.nco social.nco payouts 
+{
+  "rows": [{
+      "payouts": [{
+          "account": "socialowner1",
+          "percentage": "0.50000000000000000"
+        },{
+          "account": "socialowner2",
+          "percentage": "0.50000000000000000"
+        }
+      ],
+      "supported_symbols": [{
+          "sym": "4,NCO",
+          "contract": "eosio.token"
+        }
+      ]
+    }
+  ],
+  "more": false,
+  "next_key": ""
+}
+```
+
+***
+
+# getBalances
+
+### Parameters:
+
+### Description:
+
+Return payouts object.
+
+### Cleos Example:
+
+```
+
+cleos -u https://nodeos.newcoin.org get table social.nco socialowner1 balances
+{
+  "rows": [{
+      "owner": "socialowner1",
+      "assets": [{
+          "quantity": "0.0000 NCO",
+          "contract": "eosio.token"
+        }
+      ]
+    }
+  ],
+  "more": false,
+  "next_key": ""
+}
+```
+
+***
--- test/rpc/rpcapi.test.ts
+++ test/rpc/rpcapi.test.ts
@@ -1,6 +1,7 @@
 import { doesNotMatch } from "assert";
 import { expect } from "chai";
 import RpcApi from "../../src/api/index";
+import { SocializerPayload } from "../../src/interfaces/socializer.interface";
 
 // tslint:disable-next-line:no-var-requires
 const fetch = require("node-fetch");
@@ -11,20 +12,28 @@ describe("RPC API", () => {
   const exampleOwner1: string = "socialowner1";
   const exampleOwner2: string = "socialowner2";
 
+  const exampleSocializer1: SocializerPayload = {
+    owner: exampleOwner1,
+  };
+
+  const exampleSocializer2: SocializerPayload = {
+    owner: exampleOwner2,
+  };
+
   it("fetch payouts", async () => {
     const response = await api.getPayouts();
     const json = await response.json();
     console.log(json);
   }).timeout(2000);
 
-  it("fetch payouts:" + exampleOwner1, async () => {
-    const response = await api.getPayouts();
+  it("fetch balances:" + exampleOwner1, async () => {
+    const response = await api.getBalances(exampleSocializer1);
     const json = await response.json();
     console.log(json);
   }).timeout(2000);
 
-  it("fetch payouts:" + exampleOwner2, async () => {
-    const response = await api.getPayouts();
+  it("fetch balances:" + exampleOwner2, async () => {
+    const response = await api.getBalances(exampleSocializer2);
     const json = await response.json();
     console.log(json);
   }).timeout(2000);
